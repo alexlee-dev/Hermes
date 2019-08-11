3cd7c78f50a8b8409a01d5e60aa9c8fce6179f96
alexlee-dev <alex@alexlee.dev>
2019-08-10 23:04:41 -0700

    🔧 Add Prettier config.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import * as Sentry from '@sentry/browser';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import * as Sentry from '@sentry/browser'
 
-Sentry.init({dsn: "https://f0118327f72d4186ace6ce17ecd278c9@sentry.io/1527426"});
+Sentry.init({
+  dsn: 'https://f0118327f72d4186ace6ce17ecd278c9@sentry.io/1527426'
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
