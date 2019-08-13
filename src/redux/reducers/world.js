17d192a8c6cebc5ec48368890609d899c7c6ee14
alexlee-dev <alex@alexlee.dev>
2019-08-12 22:41:57 -0700

    ✏️ Use LocalStorage.

--- src/redux/reducers/world.js
+++ src/redux/reducers/world.js
@@ -4,11 +4,10 @@ const worldDefaultState = {
 
 export default (state = worldDefaultState, action) => {
   switch (action.type) {
-    case 'STORE_PLANET':
-      const { planet } = action.payload
-      const planetsCopy = Array.from(state.planets)
-      planetsCopy.push(planet)
-      return Object.assign({}, worldDefaultState, { planets: planetsCopy })
+    case 'STORE_PLANETS':
+      const { planets } = action.payload
+
+      return Object.assign({}, worldDefaultState, { planets })
     default:
       return state
   }
