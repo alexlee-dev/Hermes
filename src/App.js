4131cb8d2e4a711673461f968cf18c96832e3810
alexlee-dev <alex@alexlee.dev>
2019-08-18 13:26:43 -0700

    ✨ Add ability to view ship cargo contents.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { generatePlanets } from './util'
 import useLocalStorage from './hooks/useLocalStorage'
 import { storePlanets } from './redux/actions/world'
 import ItemTimer from './components/ItemTimer'
 
-const App = ({ dispatch, world }) => {
+const App = ({ dispatch, ship, world }) => {
   const [storagePlanets, setStoragePlanets] = useLocalStorage('planets', [])
 
   useEffect(() => {
@@ -20,7 +21,8 @@ const App = ({ dispatch, world }) => {
     // eslint-disable-next-line
   }, [])
 
-  const { planets } = world
+  // const { planets } = world
+  const { cargo } = ship
 
   return (
     <div>
@@ -28,18 +30,32 @@ const App = ({ dispatch, world }) => {
       <ItemTimer />
       <br />
       <div>
-        {planets.map(({ items, isHomePlanet, name }) => (
+        {/* {planets.map(({ items, isHomePlanet, name }) => (
           <div key={name}>
             <h2>{isHomePlanet ? name + ' - Home Planet' : name}</h2>
             <span>Items:</span>
             <pre>{JSON.stringify(items, null, 2)}</pre>
           </div>
-        ))}
+        ))} */}
+
+        <h2>Your Ship</h2>
+        <h3>Cargo</h3>
+        <ul>
+          {cargo.map(({ name }, i) => (
+            <li key={i}>{name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
 }
 
-const mapStateToProps = ({ world }) => ({ world })
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  ship: PropTypes.object.isRequired,
+  world: PropTypes.object.isRequired
+}
+
+const mapStateToProps = ({ ship, world }) => ({ ship, world })
 
 export default connect(mapStateToProps)(App)
