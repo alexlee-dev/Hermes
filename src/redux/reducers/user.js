b3b55c8c8f90fe9f71c4948979d4a67fd376333f
alexlee-dev <alex@alexlee.dev>
2019-08-21 13:34:41 -0700

    ✨ User is able to sell cargo by visiting planets.

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -4,6 +4,8 @@ const userDefaultState = {
 
 export default (state = userDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_CASH':
+      return { ...state, cash: state.cash + action.payload.amount }
     default:
       return state
   }
