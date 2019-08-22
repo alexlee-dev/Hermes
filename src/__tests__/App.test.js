32b4e9b42204188f2ac7b1df7b15dc16b53a251f
alexlee-dev <alex@alexlee.dev>
2019-08-21 23:17:45 -0700

    ✅ Fix 1st test file.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,46 @@
-describe('Fake Test', () => {
-  it('Should pass.', () => {
-    expect(1 + 1).toBe(2)
+/**
+ * @jest-environment node
+ */
+
+import React from 'react'
+import App from '../App'
+import { shallow } from 'enzyme'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+const defaultState = {
+  ship: {
+    cargo: [],
+    location: {
+      name: null,
+      value: null
+    }
+  },
+  ui: {
+    view: 'Ship'
+  },
+  user: {
+    cash: 100
+  },
+  world: {
+    isTimerRunning: false,
+    planets: []
+  }
+}
+
+const rerenderShallow = props =>
+  shallow(
+    <Provider store={mockStore(defaultState)}>
+      <App {...props} />
+    </Provider>
+  )
+
+describe('<App />', () => {
+  it('Should render the <App /> component.', () => {
+    const wrapper = rerenderShallow()
+    expect(wrapper.html()).toMatchSnapshot()
   })
 })
