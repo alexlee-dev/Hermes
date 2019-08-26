0d4d98f7fabe12abc93b2721bdb9fd43fde7bfed
alexlee-dev <alex@alexlee.dev>
2019-08-25 22:57:39 -0700

    ✅ Add Cypress and 1 Cypress test.

---
+++ cypress/integration/Travel.spec.js
@@ -0,0 +1,9 @@
+describe('Travel', () => {
+  it('Should travel from the Home Planet to another planet.', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('select').select('Planets')
+    cy.get(
+      '#root > div > div:nth-child(6) > div:nth-child(2) > div.StyledBox-sc-13pk1d4-0.ffeAql > button'
+    ).click()
+  })
+})
