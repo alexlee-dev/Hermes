e1fd4c5223e9d168ff41f51d305e02892df2df9c
alexlee-dev <alex@alexlee.dev>
2019-08-27 15:29:11 -0700

    ✅ Edit Travis config to run Cypress.

--- cypress/integration/Travel.spec.js
+++ cypress/integration/Travel.spec.js
@@ -3,7 +3,7 @@ describe('Travel', () => {
     cy.visit('http://localhost:3000')
     cy.get('select').select('Planets')
     cy.get(
-      '#root > div > div:nth-child(6) > div:nth-child(2) > div.StyledBox-sc-13pk1d4-0.ffeAql > button'
+      '#root > div > div:nth-child(6) > div:nth-child(2) > div.StyledBox-sc-13pk1d4-0.gQLnBt > button'
     ).click()
   })
 })
