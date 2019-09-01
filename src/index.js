f6b9aeb8836d21e2ef7874bd9b93ef8015d4597d
alexlee-dev <alex@alexlee.dev>
2019-08-31 17:51:42 -0700

    ✏️ Only init GA/LogRocket/Sanity if in production.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ import * as Sentry from '@sentry/browser'
 import { Provider } from 'react-redux'
 import store from './redux/store/store'
 import LogRocket from 'logrocket'
+import ReactGA from 'react-ga'
 
-Sentry.init({
-  dsn: 'https://f0118327f72d4186ace6ce17ecd278c9@sentry.io/1527426'
-})
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://f0118327f72d4186ace6ce17ecd278c9@sentry.io/1527426'
+  })
 
-LogRocket.init('alex-lee/hermes-vjulr')
+  LogRocket.init('alex-lee/hermes-vjulr')
+
+  ReactGA.initialize('UA-124507483-6')
+  ReactGA.pageview(window.location.pathname + window.location.search)
+}
 
 ReactDOM.render(
   <Provider store={store}>
